290fd5783675662d217a507af543f0777c44e42e
meharkp7 <meharkapoor7@gmail.com>
2026-09-20 02:03:45 +0530

    loveda config files

---
+++ geospatial/change/__init__.py
@@ -0,0 +1,11 @@
+from geospatial.change.detector import (
+    ChangeDetectionError,
+    ChangeSummary,
+    detect_semantic_change,
+)
+
+__all__ = [
+    "ChangeDetectionError",
+    "ChangeSummary",
+    "detect_semantic_change",
+]
