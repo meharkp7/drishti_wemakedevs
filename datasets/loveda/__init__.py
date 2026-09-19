a37653fe140308074acf926e9559971294987215
meharkp7 <meharkapoor7@gmail.com>
2026-09-20 01:59:04 +0530

    feat(datasets): LoveDA dataset, transforms, validator, Kaggle download helper

--- datasets/loveda/__init__.py
+++ datasets/loveda/__init__.py
@@ -1,5 +1,10 @@
 """DRISHTI LoveDA dataset package -- the ML Data Layer's LoveDA data source."""
 
+from datasets.loveda.dataset import (
+    LoveDADataset,
+    LoveDADatasetError,
+    LoveDASampleItem,
+)
 from datasets.loveda.manifest import (
     DOMAINS,
     SPLITS,
@@ -9,7 +14,6 @@ from datasets.loveda.manifest import (
     ManifestStats,
     scan_dataset,
 )
-
 from datasets.loveda.taxonomy import (
     IGNORE_INDEX,
     LOVEDA_CLASSES,
@@ -21,8 +25,12 @@ from datasets.loveda.taxonomy import (
     class_names,
     is_valid_mask_value,
 )
+from datasets.loveda.transforms import eval_transform, train_transform
 
 __all__ = [
+    "LoveDADataset",
+    "LoveDADatasetError",
+    "LoveDASampleItem",
     "DOMAINS",
     "SPLITS",
     "LoveDAManifest",
@@ -39,4 +47,6 @@ __all__ = [
     "class_by_name",
     "class_names",
     "is_valid_mask_value",
+    "eval_transform",
+    "train_transform",
 ]
